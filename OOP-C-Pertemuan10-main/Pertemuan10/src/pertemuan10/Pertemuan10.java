4f4ce6b97f80493b3936f176c420e1c877e09d34
viraprilia <viraguman@gmail.com>
2025-06-05 14:47:16 +0800

    Add files via upload

---
+++ OOP-C-Pertemuan10-main/Pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,17 @@
+package pertemuan10;
+
+/**
+ *
+ * @author Wihelmina Aprilia
+ * TGL: 24 Mei 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
